d2bfada3e267336b4c3e9f1927fdfdec29b5e8f9
Jenkin Schibel <jenkin.schibel@protonmail.com>
2020-03-10 18:52:35 -0700

    can generate population with random values

--- Rando.py
+++ Rando.py
@@ -30,7 +30,19 @@ class AIPlayer(Player):
     ##
     def __init__(self, inputPlayerId):
         super(AIPlayer,self).__init__(inputPlayerId, "Rando")
-    
+
+
+    ##
+    #mateGenes
+    #parameters:
+    # daddyGene: the first gene
+    # mommyGene: the second gene
+    #description:
+    # creates 2 child genes after mating 
+    # and mutating from parent genes
+    def mateGenes(daddyGene,mommyGene):
+        pass
+
     ##
     #getPlacement
     #
@@ -46,44 +58,56 @@ class AIPlayer(Player):
     #Return: The coordinates of where the construction is to be placed
     ##
     def getPlacement(self, currentState):
-        numToPlace = 0
-        #implemented by students to return their next move
+        # numToPlace = 0
+        # #implemented by students to return their next move
         if currentState.phase == SETUP_PHASE_1:    #stuff on my side
-            numToPlace = 11
-            moves = []
-            for i in range(0, numToPlace):
-                move = None
-                while move == None:
-                    #Choose any x location
-                    x = random.randint(0, 9)
-                    #Choose any y location on your side of the board
-                    y = random.randint(0, 3)
-                    #Set the move if this space is empty
-                    if currentState.board[x][y].constr == None and (x, y) not in moves:
-                        move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                        currentState.board[x][y].constr == True
-                moves.append(move)
-            return moves
+            # numToPlace = 11
+            # moves = []
+            # for i in range(0, numToPlace):
+                # move = None
+                # while move == None:
+                    # #Choose any x location
+                    # x = random.randint(0, 9)
+                    # #Choose any y location on your side of the board
+                    # y = random.randint(0, 3)
+                    # #Set the move if this space is empty
+                    # if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        # move = (x, y)
+                        # #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        # currentState.board[x][y].constr == True
+                # moves.append(move)
+            # return moves
+
+            moves = [] 
+            moves.append(Gene.geneList[Gene.geneIndex].hillLoc)
+            moves.append(Gene.geneList[Gene.geneIndex].tunnelLoc)
+            for grass in Gene.geneList[Gene.geneIndex].grassLocs:
+                moves.append(grass)
+            return moves 
+            
         elif currentState.phase == SETUP_PHASE_2:   #stuff on foe's side
-            numToPlace = 2
-            moves = []
-            for i in range(0, numToPlace):
-                move = None
-                while move == None:
-                    #Choose any x location
-                    x = random.randint(0, 9)
-                    #Choose any y location on enemy side of the board
-                    y = random.randint(6, 9)
-                    #Set the move if this space is empty
-                    if currentState.board[x][y].constr == None and (x, y) not in moves:
-                        move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                        currentState.board[x][y].constr == True
-                moves.append(move)
+            # numToPlace = 2
+            # moves = []
+            # for i in range(0, numToPlace):
+                # move = None
+                # while move == None:
+                    # #Choose any x location
+                    # x = random.randint(0, 9)
+                    # #Choose any y location on enemy side of the board
+                    # y = random.randint(6, 9)
+                    # #Set the move if this space is empty
+                    # if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        # move = (x, y)
+                        # #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        # currentState.board[x][y].constr == True
+                # moves.append(move)
+            # return moves
+        # else:
+            # return [(0, 0)]
+            moves = [] 
+            moves.append(Gene.geneList[Gene.geneIndex].foodLocs[0])
+            moves.append(Gene.geneList[Gene.geneIndex].foodLocs[1])
             return moves
-        else:
-            return [(0, 0)]
     
     ##
     #getMove
@@ -126,3 +150,140 @@ class AIPlayer(Player):
     def registerWin(self, hasWon):
         #method templaste, not implemented
         pass
+
+class Gene():
+    
+    POPULATION_SIZE = 2
+    NUM_EVALS = 2
+    NUM_GRASS = 9 
+    NUM_FOOD = 2
+
+    geneList = []
+    geneIndex = 0
+
+    def __init__(self,hillLoc=None,tunnelLoc=None,grassLocs=None,eFoodLocs=None,fitness=0,numEvals=0):
+        self.hillLoc = hillLoc
+        self.tunnelLoc = tunnelLoc
+        self.grassLocs = grassLocs
+        self.FoodLocs = eFoodLocs
+        self.fitness = fitness
+        self.numEvals = numEvals
+        self.occupiedSpots = []
+
+    def toString(self):
+
+        output = "hill: {} \n tunnel: {} \n grass: {} \n food: {} \n fitness: {} \n evals: {} \n spots: {}".format(self.hillLoc,self.tunnelLoc,join(map(str,self.grassLocs)),join(map(str,self.foodLocs)),self.fitness,self.numEvals)
+
+
+    def mutate(self):
+        pass
+
+    ##
+    #makeBabies
+    #parameters:
+    # daddyGene: the first gene
+    # mommyGene: the second gene
+    #description:
+    # creates 2 child genes after mating 
+    # and mutating from parent genes
+    @staticmethod
+    def makeBabies(daddyGene,mommyGene):
+        brother = Gene()
+        sister = Gene()
+
+        hillGeneBrother = random.random()
+        hillGeneSister = random.random()
+        tunnelGeneBrother = random.random()
+        tunnelGeneSister = random.random()
+        #get the split value to dispere the mommy and daddy genes
+
+
+    @staticmethod
+    def makeNewPopulation(oldPeople): 
+        newPeople = None
+
+
+    ##
+    #populationInit
+    # parameters:
+    # self
+    #Description:
+    # randomnly generates gene attribute values for the start of the learning session
+    # and resets all fitness values to 0
+    @staticmethod
+    def populationInit():
+        occupiedSpots = []
+
+        for x in range(Gene.POPULATION_SIZE): 
+
+            hillPlacement = Gene.pickASpotMe() 
+            occupiedSpots.append(hillPlacement)
+
+
+            tunnelPlacement = Gene.pickASpotMe()
+            while tunnelPlacement in occupiedSpots:
+                tunnelPlacement = Gene.pickASpotMe()
+            occupiedSpots.append(tunnelPlacement)
+
+            grassCoords = []
+            for x in range(Gene.NUM_GRASS):
+                grassPlacement = Gene.pickASpotMe() 
+                while grassPlacement in occupiedSpots:
+                    grassPlacement = Gene.pickASpotMe()
+                grassCoords.append(grassPlacement) 
+                occupiedSpots.append(grassPlacement)
+
+            foodCoords = []
+            for x in range(Gene.NUM_FOOD):
+                foodPlacement = Gene.pickASpotEnemy()
+                while foodPlacement in occupiedSpots:
+                    foodPlacement = Gene.pickASpotEnemy()
+                foodCoords.append(foodPlacement) 
+                occupiedSpots.append(foodPlacement)
+
+            Gene.geneList.append(Gene(hillPlacement,tunnelPlacement,grassCoords,foodCoords))
+
+    
+    @staticmethod
+    def pickASpotMe():
+            return (random.randint(0,9),random.randint(0,3))
+
+    @staticmethod
+    def pickASpotEnemy():
+            return (random.randint(0,9),random.randint(6,9)) 
+
+    def locationUsed(coords):
+        if coords in self.occupiedSpots:
+            return True
+        else:
+            return False
+
+
+class MyUtils(): 
+
+    @staticmethod
+    def spotTaken(currentState):
+        if currentState.board[x][y].constr == None :
+            return True
+        else:
+            return False
+
+    ## findClosestEmpty
+    @staticmethod
+    def findClosestEmpty(x,y,currestState,whosSide="me"):
+        if whosSide=="me":
+            miny = 0
+            maxy = 3
+        else:
+            miny = 6
+            maxy = 9
+
+
+
+
+        
+
+
+        
+
+Gene.populationInit()
